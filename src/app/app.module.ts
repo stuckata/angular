35afc418bbf6738b5d1fb02a6c88bf8fd7b93220
Stoyan Kirkov <stu.kirkov@gmail.com>
2019-01-03 14:17:39 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { MarkComponent } from './mark/mark.component';
+import { SubjectComponent } from './subject/subject.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { SchoolClassComponent } from './school-class/school-class.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    MarkComponent,
+    SubjectComponent,
+    TeacherComponent,
+    SchoolClassComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
