44b6b73bb99a5f36340168440f06b3d4ddc08b09
Stoyan Kirkov <stu.kirkov@gmail.com>
2019-01-19 14:12:50 +0200

    dropdowns for class and subject added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { SchoolClass } from '../school-classes/school-class.model';
+import { Subject } from '../subjects/subject.model';
+import { SchoolClassService } from '../services/school-class.service';
+import { SubjectService } from '../services/subject.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  classes: SchoolClass[];
+  subjects: Subject[];
+
+  constructor(private schoolClassService: SchoolClassService, private subjectService: SubjectService) { }
 
   ngOnInit() {
+    this.classes = this.schoolClassService.getClasses();
+    this.schoolClassService.classesChanged.subscribe((classes: SchoolClass[]) => this.classes = classes);
+    this.subjects = this.subjectService.getSubjects();
+    this.subjectService.subjectsChanged.subscribe((subjects: Subject[]) => this.subjects = subjects);
   }
 
+  onClassChange(id: number) {
+    console.log('onClassChange => id: ' + id);
+  }
+
+  onSubjectChange(id: number) {
+    console.log('onSubjectChange => id: ' + id);
+  }
 }
