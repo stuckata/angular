e7d4d33c8c81f720f50b48ba6cf0e1e375408279
Stoyan Kirkov <stu.kirkov@gmail.com>
2019-01-27 10:26:50 +0200

    student service refactor

--- src/app/services/school-class.service.ts
+++ src/app/services/school-class.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { SchoolClass } from '../school-classes/school-class.model';
--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,38 +1,19 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { Student } from '../students/student.model';
+import { RestService } from './rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StudentService {
+export class StudentService extends RestService {
 
-  studentsChanged = new EventEmitter<Student[]>();
-
-  endpoint = 'http://localhost:8080/api/v1/';
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  };
-
-  constructor(private http: HttpClient) { }
-
-  extractData(res: Response) {
-    let body = res;
-    return body || {};
-  }
+  private relativeUrl: string = 'students';
 
   getStudents(): Observable<any> {
-    return this.http.get(this.endpoint + 'students').pipe(
-      map(this.extractData));
+    return this.get(this.relativeUrl);
   }
 
   getStudentById(id: number): Observable<any> {
-    return this.http.get(this.endpoint + 'students/' + id).pipe(
-      map(this.extractData));
+    return this.getById(this.relativeUrl, id);
   }
 }
